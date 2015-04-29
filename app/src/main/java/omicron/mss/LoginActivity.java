23962195a60fd4e33df12991d1e143672fe7807d
Rige02 <robert.aron.tx@verizon.net>
2015-04-29 17:31:06 -0500

    Don't worry about this
    This stuff doesn't work

--- app/src/main/java/omicron/mss/LoginActivity.java
+++ app/src/main/java/omicron/mss/LoginActivity.java
@@ -1,7 +1,8 @@
 package omicron.mss;
 
-import android.support.v7.app.ActionBarActivity;
+import android.R;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 
