2b39c2f589c960d9b89e79d26a8aa492e6ff400a
Rige02 <robert.aron.tx@verizon.net>
2015-05-06 14:58:50 -0500

    updated logo stuff
    logo stuff

--- app/src/main/java/omicron/mss/ScheduleCreator.java
+++ app/src/main/java/omicron/mss/ScheduleCreator.java
@@ -1,12 +1,16 @@
 package omicron.mss;
 
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
+
 /**
  * Created by Ian on 5/4/2015.
  */
 public class ScheduleCreator {
 
     public ScheduleCreator(){
-        //can't finish yet
+        ParseQuery<ParseObject> query = ParseQuery.getQuery("ClassDB");
+
     }
 
 
